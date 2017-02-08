9d00c718b3b548802a2705b55c3adb690854bf75
asmln <a.smln@yandex.ru>
2017-02-08 23:38:26 +0300

    Добавил поиск в глубину.

--- src/main/scala/Graph.scala
+++ src/main/scala/Graph.scala
@@ -6,20 +6,52 @@ import scala.util.Random
 class Graph(nodesCount: Int) {
 
   val matrix: Array[Array[Byte]] = Array.ofDim[Byte](nodesCount, nodesCount)
-  val nodes: Array[Boolean] = Array.ofDim[Boolean](nodesCount)
 
-  def randomInit(): Unit = {
+  def this(matrix: Array[Array[Byte]]) {
+    this(matrix.length)
     for(i <- 0 until nodesCount) {
       for(j <- 0 until nodesCount) {
-        matrix(i)(j) = Random.nextInt(2).asInstanceOf[Byte]
+        if (matrix(i)(j) == 1) this.addRoute(i, j)
       }
-      nodes(i) = false
+    }
+  }
+
+  def addRoute(a: Int, b:Int): Unit = {
+    matrix(a)(b) = 1
+  }
+
+  def randomInit(n: Int): Unit = {
+    for(i <- 0 until nodesCount) {
+      for(j <- 0 until nodesCount) {
+        matrix(i)(j) = if (Random.nextInt(n) == 0) 1 else 0
+      }
     }
   }
 
   def checkPath(a: Int, b: Int): Boolean = {
-    false
+    if (a == b) {
+      matrix(a)(b) == 1
+    } else {
+      checkPath(a, b, Array.ofDim[Byte](nodesCount))
+    }
   }
+
+  private def checkPath(a: Int, b: Int, nodes: Array[Byte]): Boolean =
+    if (a == b) {
+      true
+    } else {
+      nodes(a) = 1
+      var found = false
+      var i = 0
+      while ((i < nodesCount) && !found) {
+        if (matrix(a)(i) == 1 && nodes(i) == 0) {
+          found = checkPath(i, b, nodes)
+        }
+        i += 1
+      }
+      found
+    }
+
 
   override def toString: String = {
     matrix.deep.mkString("\n")
@@ -28,4 +60,5 @@ class Graph(nodesCount: Int) {
 
 object Graph {
   def apply(nodesCount: Integer): Graph = new Graph(nodesCount)
+  def apply(matrix: Array[Array[Byte]]): Graph = new Graph(matrix)
 }
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,8 +3,30 @@
   */
 object Main {
   def main(args: Array[String]): Unit = {
-    val graph = Graph(10)
-    graph.randomInit()
+    val randomGraph = Graph(10)
+    randomGraph.randomInit(6)
+    println(randomGraph)
+    println(randomGraph.checkPath(0,0))
+    println(randomGraph.checkPath(0,1))
+    println(randomGraph.checkPath(0,2))
+    println(randomGraph.checkPath(0,3))
+    println(randomGraph.checkPath(0,4))
+    println(randomGraph.checkPath(0,9))
+
+    val matrix: Array[Array[Byte]] = Array(
+      Array(0.toByte, 1.toByte, 0.toByte, 0.toByte),
+      Array(0.toByte, 0.toByte, 1.toByte, 0.toByte),
+      Array(0.toByte, 0.toByte, 0.toByte, 0.toByte),
+      Array(0.toByte, 0.toByte, 1.toByte, 0.toByte)
+    )
+    val graph = Graph(matrix)
+
     println(graph)
+    println(graph.checkPath(0,0))
+    println(graph.checkPath(0,1))
+    println(graph.checkPath(0,2))
+    println(graph.checkPath(2,1))
+    println(graph.checkPath(2,3))
+    println(graph.checkPath(0,3))
   }
 }
